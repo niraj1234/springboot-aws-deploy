e23e536a282eaf30fd80d9c1cd5902cddcf72a0d
niraj1234 <niraj.webtech@gmail.com>
2024-04-27 02:33:51 +0530

    Adding person API in test controller

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -16,4 +16,10 @@ public class TestController {
     public String getMessage() {
         return "Second message from AWS Ecs";
     }
+    
+    @GetMapping("/p")
+    public String person() {
+    	return "Hello Niraj";
+    }
+
 }
